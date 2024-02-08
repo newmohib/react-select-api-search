5f4f0003285c929d5fdac1978eaa8bed8c7136f2
bs1109 <newmohib@gmail.com>
2024-02-08 21:19:27 +0600

    modify react-select

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 import React from 'react';
 
-import AsyncSelect from 'react-select/async';
+import AsyncSelect from 'react-select/async';Select
+import Select from 'react-select';
 import { ColourOption, colourOptions, stateOptions, StateOption } from '@/app/docs/data';
 
 // import Image from "next/image";
@@ -35,8 +36,24 @@ const loadOptions = (
 export default function Home() {
   return (
     <div style={{width:"300px", margin:"300px"}}>
-      <AsyncSelect onChange= {(value)=> console.log(value)} cacheOptions loadOptions={loadOptions} defaultOptions />
-
+      {/* <AsyncSelect onChange= {(value)=> console.log(value)} cacheOptions loadOptions={loadOptions} defaultOptions /> */}
+      <Select
+        className="basic-single"
+        classNamePrefix="select"
+        defaultValue={stateOptions[0]}
+        isDisabled={false}
+        isLoading={false}
+        isClearable={true}
+        onInputChange = {(newValue, actionMeta)=>{
+          console.log(newValue, actionMeta)
+        }}
+        isSearchable={true}
+        name="color"
+        options={stateOptions}
+      />
     </div>
   );
 }
+
+
+
